9e7a70814340bae49d28d5c24e43953c5730fcc1
Khalid Al-Saif <93504189+khaldalsaif@users.noreply.github.com>
2022-03-16 23:14:16 +0300

    Add files via upload

---
+++ core.js
@@ -0,0 +1,73 @@
+$('body').terminal({
+    achiv: function () {
+        this.echo('\n\t\tCompleting Cybercraft program Solution by STC, Khobar\n'
+                +	'\t\t\t\tJuly 2018 — August 2018\n'
+                +   '\t\t1-CompTIA Network+\n'
+                +	'\t\t2-CompTIA Server+\n'
+                +	'\t\t3-CSP+\n' 
+                +	'\t\t4-CPTE\n\n\n'
+                +	'\t\tCertification of Appreciation for Exploring a critical personal information \n\t\texposure in Student Information System. \n\t\t\t\tJanuary 2022 - Dammam, IAU\n' 
+);
+    },
+    major: function () {
+        this.echo('\tCyber Security and Digital Forensics\n');
+    },
+    cert: function () {
+        this.echo('\teJPT: elearn Joniur Penetration Tester\n');
+    },
+    github: function () {
+        window.open("https://github.com/khaldalsaif");
+        this.echo("\n");
+    },
+    youtube: function () {
+        window.open("https://www.youtube.com/channel/UCyrkAOLaBAvrZvDRolwvyHg");
+        this.echo("\n");
+    },
+    linkedin: function () {
+        window.open("https://www.linkedin.com/in/khalid-s-al-saif-2951ab185/");
+        this.echo("\n");
+    },
+    cv: function () {
+        window.open("files\\Khalid's_CV_2022.pdf");
+        his.echo("\n");
+    },
+    profile: function () {
+        this.echo(
+                    '\n\t\tHello, \n'
+                +   '\t\ti\'m Khalid Saif Al-Saif.\n' 
+                +   '\t\tStudent At Imam Abdulrhaman bin Faisal University in Ad Dammam city\n'
+                +   '\t\tI am a dedicated and goal-oriented guy with a strong interest in the Cybersecurity and IOT filed.\n'
+                +	'\t\tI am adaptable, dependable, and have outstanding time management abilities.\n'
+                +	'\t\tI am a person who is energetic, self-motivated, trustworthy, responsible, and hardworking.\n'
+                +	'\t\tI am an experienced team player that can adjust to any scenario.\n'
+                +	'\t\tI can operate well in a team setting as well as on my own initiative.\n');
+    },
+    contact: function () {
+        this.echo('\t\temail:\n'+ '\t\t\tKhaldalsaif@gmail.com\n\n'+
+                  '\t\tPhone Number:\n'+'\t\t\t+966 59 141 0950\n\n'+
+                  '\t\tMailbox:\n'+'\t\t\tP.O.BOX  7772, Khobar\n\n'
+                  );
+    },
+    help: function () {
+        this.echo('\t\tprofile - Let me talk about my self please!.\n'  );
+        this.echo('\t\tcv      - Allow the pop-up to see my resume.. don\'t worry won\'t hack ya!.'  );
+        this.echo('\t\tachiv   - You can see my Achivments.');
+        this.echo('\t\tmajor   - What is my major?? hmmm.. not web devlopment obviously :(.');
+        this.echo('\t\tcert    - Display my certfcations.\n'  );
+        this.echo('\t\tcontact - Reach me. '  );
+        this.echo('\t\tgithub  - My GitHub page.'  );
+        this.echo('\t\tlinkdin - My LinkedIn page.'  );
+        this.echo('\t\tyoutube - My YouTube page.\n'  );
+    }
+    ,
+}, {
+    greetings: 
+'██╗░░██╗██╗░░██╗░█████╗░██╗░░░░░██╗██████╗░  ░█████╗░██╗░░░░░░██████╗░█████╗░██╗███████╗  ██████╗░██████╗░░█████╗░███████╗██╗██╗░░░░░███████╗\n'+
+'██║░██╔╝██║░░██║██╔══██╗██║░░░░░██║██╔══██╗  ██╔══██╗██║░░░░░██╔════╝██╔══██╗██║██╔════╝  ██╔══██╗██╔══██╗██╔══██╗██╔════╝██║██║░░░░░██╔════╝\n'+
+'█████═╝░███████║███████║██║░░░░░██║██║░░██║  ███████║██║░░░░░╚█████╗░███████║██║█████╗░░  ██████╔╝██████╔╝██║░░██║█████╗░░██║██║░░░░░█████╗░░\n'+
+'██╔═██╗░██╔══██║██╔══██║██║░░░░░██║██║░░██║  ██╔══██║██║░░░░░░╚═══██╗██╔══██║██║██╔══╝░░  ██╔═══╝░██╔══██╗██║░░██║██╔══╝░░██║██║░░░░░██╔══╝░░\n'+
+'██║░╚██╗██║░░██║██║░░██║███████╗██║██████╔╝  ██║░░██║███████╗██████╔╝██║░░██║██║██║░░░░░  ██║░░░░░██║░░██║╚█████╔╝██║░░░░░██║███████╗███████╗\n'+
+'╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═╝╚═════╝░  ╚═╝░░╚═╝╚══════╝╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░  ╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░╚═╝╚══════╝╚══════╝\n'+
+'\n\n To display the commands for this website use this command  -  help\n'
+}
+);
